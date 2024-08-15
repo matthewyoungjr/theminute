d44e487ec5133ea5e29fa2a65e1ae7e812503894
Matthew <mattyoungjnr@gmail.com>
2024-08-15 13:26:30 +0000

    Added Grid to make page items responsive.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
-import apiClient from "./services/api-client";
+import NewsList from "./compnents/NewsList";
+import SingleNewsArticle from "./compnents/SingleNewsArticle";
 import Header from "./Header";
 
 export default function Home() {
-  return <Header title="Latest News" />;
+  return (
+    <>
+      <Header title="Latest News" />
+
+      <div className="mt-10 grid grid-cols-1 lg:grid-cols-2 items-start">
+        <SingleNewsArticle />
+        <NewsList />
+      </div>
+    </>
+  );
 }
