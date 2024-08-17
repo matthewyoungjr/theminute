5e1bf78b7fdff12829e3b9a9be8777143fdf3051
Matthew <mattyoungjnr@gmail.com>
2024-08-17 12:51:02 +0000

    Create NewsGrid component.

---
+++ app/compnents/NewsGrid.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { ReactNode } from "react";
+import { Grid } from "@/components/ui/grid";
+import { GridListItem } from "react-aria-components";
+
+interface GridProp {
+  children: ReactNode;
+}
+
+const NewsGrid = ({ children }: GridProp) => {
+  return (
+    <Grid
+      columns={{ initial: 1, sm: 2, md: 3, xl: 4 }}
+      gapX={5}
+      gapY={7}
+      className="justify-items-center p-7"
+    >
+      {children}
+    </Grid>
+  );
+};
+
+export default NewsGrid;
