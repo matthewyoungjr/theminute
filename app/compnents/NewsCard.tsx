cd2a00862558c12cb5cdd86d6b4c2f1fb8ca46dd
Matthew <mattyoungjnr@gmail.com>
2024-08-17 13:44:44 +0000

    Add hover colors using clsx

--- app/compnents/NewsCard.tsx
+++ app/compnents/NewsCard.tsx
@@ -2,11 +2,20 @@
 import React from "react";
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
+import { useTheme } from "next-themes";
+import clsx from "clsx";
 
 const NewsCard = () => {
+  const { resolvedTheme } = useTheme();
+
+  const bg = clsx(
+    { "gray-200": resolvedTheme === "light" },
+    { "gray-900": resolvedTheme === "dark" }
+  );
+
   return (
     <Link href="">
-      <Card className="max-w-xs">
+      <Card className={`max-w-x hover:bg-` + bg}>
         <Card.Header>
           <Card.Title> :Business </Card.Title>
         </Card.Header>
