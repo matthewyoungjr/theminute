11127194b3944101ad49d90daec10d6c808b868f
Matthew <mattyoungjnr@gmail.com>
2024-08-18 06:19:34 +0000

    Create Article Title component.

---
+++ app/[newsTitle]/ArticleTitle.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Heading } from "@/components/ui/heading";
+
+const ArticleTitle = () => {
+  return (
+    <Heading
+      level={2}
+      tracking="wider"
+      className="text-wrap mx-2 px-4 mt-3 max-w-2xl md:mx-auto md:text-xl lg:text-2xl"
+    >
+      Elenas: The Ouest to Become LatAm's Social Commerce Leader And Plans On
+      Becoming A Dictator.
+    </Heading>
+  );
+};
+
+export default ArticleTitle;
