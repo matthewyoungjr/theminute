72557e048b56d1b1d9c6c45ae5577a590bb455f3
Matthew <mattyoungjnr@gmail.com>
2024-08-19 05:28:31 +0000

    Add opensans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Open_Sans } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../components/providers";
 import NextTopLoader from "nextjs-toploader";
 import NavBar from "./NavBar";
 import QueryClientProvider from "./QueryClientProvider";
-
 const inter = Inter({ subsets: ["latin"] });
 
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
 export const metadata: Metadata = {
   title: "The Minute",
   description: "Access to news articles from all over the world.",
