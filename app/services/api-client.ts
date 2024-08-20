76943f2f12f0c8c3b47fedfbd0e3121852a6e39f
Matthew <mattyoungjnr@gmail.com>
2024-08-20 18:04:03 +0000

    Changed api to news api

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://newsapi.org/v2/",
+  baseURL: "https://newsapi.org/v2",
   params: {
     apiKey: "3cd8914b298e4dc687c1c16434214127",
+    pageSize: 12,
   },
 });
