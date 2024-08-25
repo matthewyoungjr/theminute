0e96380618e17ac8c2605a7ac1851e5ce359f227
Matthew <mattyoungjnr@gmail.com>
2024-08-25 06:08:53 +0000

    refactor: change fetch function name, add basic error handling.

--- app/hooks/useTopHeadlines.ts
+++ app/hooks/useTopHeadlines.ts
@@ -3,16 +3,19 @@ import apiClient from "../services/api-client";
 import { Response } from "../types/newsType";
 
 export const useTopHeadlines = () => {
-  const fetchGeneral = () => {
+  const fetchHeadlines = () => {
     return apiClient
       .get<Response>("/top-headlines?country=fr")
       .then((res) => res.data.articles)
-      .catch((error) => console.error(error.message));
+      .catch((error) => {
+        console.error(error.message);
+        throw error;
+      });
   };
 
   const { data, error, isLoading } = useQuery({
     queryKey: ["top-headlines"],
-    queryFn: fetchGeneral,
+    queryFn: fetchHeadlines,
     retry: 2,
     staleTime: 60000, // 60 seconds
   });
