300b2aa849b1dc68a37ce09e60589eb45776eb72
Matthew <mattyoungjnr@gmail.com>
2024-08-25 06:27:25 +0000

    delete imports

--- app/[newsTitle]/page.tsx
+++ app/[newsTitle]/page.tsx
@@ -1,11 +1,3 @@
-import { useSingle } from "../hooks/useSingle";
-import { newsTitleProps } from "../types/newsType";
-import ArticleImage from "./ArticleImage";
-import ArticlePublisherInfo from "./ArticlePublisherInfo";
-import ArticleTitle from "./ArticleTitle";
-import ContentArea from "./ContentArea";
-import apiClient from "../services/api-client";
-
 type Props = {
   searchParams: {
     q: string;
