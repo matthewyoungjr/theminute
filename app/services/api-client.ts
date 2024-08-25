3def5ca4d69506115e3418634c82027089e22b8f
Matthew <mattyoungjnr@gmail.com>
2024-08-25 08:26:03 +0000

    add CORS proxyURL

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+
 export default axios.create({
-  baseURL: "https://newsapi.org/v2",
+  baseURL: `${proxyUrl}https://newsapi.org/v2`,
   params: {
     apiKey: "3cd8914b298e4dc687c1c16434214127",
     pageSize: 12,
