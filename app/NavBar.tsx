49a502dfc4fd1744f34f53ecaab1e47d2ab942e9
Matthew <mattyoungjnr@gmail.com>
2024-08-31 15:38:40 +0000

    Add web app name to navbar.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,14 @@
-"use client";
-import React from "react";
 import { navLinks } from "./routes/nav-links";
 import { Link } from "next-view-transitions";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between border-b p-2 items-center shadow-md">
+    <nav className="flex justify-between border-b p-2 items-center shadow-sm">
+      <span className="font-semibold text-lg ml-5 md:text-base">
+        The Minute
+      </span>
+
       <ul className="flex space-x-5 items-center">
         {navLinks.map((link, index) => (
           <li
