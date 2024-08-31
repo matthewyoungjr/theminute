3bdbf86747b4a55e8ab1303b0c8f9d763fa3b038
Matthew <mattyoungjnr@gmail.com>
2024-08-31 07:32:10 +0000

    delete proxyurl

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-
 export default axios.create({
-  baseURL: `${proxyUrl}https://newsapi.org/v2`,
+  baseURL: "https://newsapi.org/v2",
   params: {
     apiKey: "3cd8914b298e4dc687c1c16434214127",
     pageSize: 12,
