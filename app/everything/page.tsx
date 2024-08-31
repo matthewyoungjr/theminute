dfb70b6b6ce2b359924c4d66170c14d6dfb8d8c9
Matthew <mattyoungjnr@gmail.com>
2024-08-31 15:20:38 +0000

    styles: centered header on mobile.

--- app/everything/page.tsx
+++ app/everything/page.tsx
@@ -1,11 +1,12 @@
 import { Heading } from "@/components/ui/heading";
-import NewsFilter from "../compnents/NewsFilter";
 import Search from "../compnents/Search";
+import NewsGrid from "../compnents/NewsGrid";
+import useEverything from "../hooks/useEverything";
 
 const Everything = () => {
   return (
     <>
-      <Heading level={1} className="text-6xl mt-10 ml-5 md:text-center">
+      <Heading level={1} className="text-center text-6xl mt-10 ml-5">
         Everything
       </Heading>
       <Search />
