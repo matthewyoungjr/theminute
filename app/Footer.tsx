86c4d00162a59d8e68de7575e1b6ad8738e31d86
Matthew Jnr <mattyoungjnr@gmail.com>
2024-09-26 15:59:37 +0000

    Added other socials to Footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 export default function Footer () {
       return (
             <>
             <footer className='w-full block text-center'>
                   <p className="text-center text-base md:text-lg xl:text-xl">Made with ❤️ by Matthew.</p>
-                  <p className="text-center text-base md:text-lg xl:text-xl"> Email : mattyoungjnr@gmail.com</p>
+                  <p className="text-center text-base md:text-lg xl:text-xl"><Link href="https://github.com/matthewyoungjr" target="_blank">Github</Link></p>
+                  <p className="text-center text-base md:text-lg xl:text-xl"><Link href="mailto:mattyoungjnr@gmail.com" target="_blank">Email</Link></p>
+                  <p className="text-center text-base md:text-lg xl:text-xl"><Link href="https://x.com/themattjnr" target="_blank">X / Twitter</Link></p>
             </footer>
             </>
       )
