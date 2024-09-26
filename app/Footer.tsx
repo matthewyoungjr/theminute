4a72bdbf3fab86883c47733c42c94cadeb01321e
Matthew Jnr <mattyoungjnr@gmail.com>
2024-09-26 16:06:54 +0000

    add spacing between socials

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,10 +3,13 @@ export default function Footer () {
       return (
             <>
             <footer className='w-full block text-center'>
+                  <div className="flex space-x-5">
                   <p className="text-center text-base md:text-lg xl:text-xl">Made with ❤️ by Matthew.</p>
                   <p className="text-center text-base md:text-lg xl:text-xl"><Link href="https://github.com/matthewyoungjr" target="_blank">Github</Link></p>
                   <p className="text-center text-base md:text-lg xl:text-xl"><Link href="mailto:mattyoungjnr@gmail.com" target="_blank">Email</Link></p>
                   <p className="text-center text-base md:text-lg xl:text-xl"><Link href="https://x.com/themattjnr" target="_blank">X / Twitter</Link></p>
+
+                  </div>
             </footer>
             </>
       )
