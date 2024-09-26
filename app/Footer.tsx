12e1744de81dad0bb238dc1fda7243bb455fa74c
Matthew Jnr <mattyoungjnr@gmail.com>
2024-09-26 16:12:37 +0000

    center socials

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -4,11 +4,10 @@ export default function Footer () {
             <>
             <footer className='w-full block text-center'>
                   <p className="text-center text-base md:text-lg xl:text-xl">Made with ❤️ by Matthew.</p>
-                  <div className="flex space-x-5 text-center underline">
+                  <div className="flex space-x-5 justify-center underline">
                   <p className="text-center text-base md:text-lg xl:text-xl"><Link href="https://github.com/matthewyoungjr" target="_blank">Github</Link></p>
                   <p className="text-center text-base md:text-lg xl:text-xl"><Link href="mailto:mattyoungjnr@gmail.com" target="_blank">Email</Link></p>
                   <p className="text-center text-base md:text-lg xl:text-xl"><Link href="https://x.com/themattjnr" target="_blank">X / Twitter</Link></p>
-
                   </div>
             </footer>
             </>
